8fbf41b2cf90487a37e3114ac91fcb3dd61b5919
Ömer Faruk Kocadayı <omerkocadayi@gmail.com>
2017-03-26 01:17:03 +0200

    Update 1-print-kullanim-sekilleri.py

--- 1-print-kullanim-sekilleri.py
+++ 1-print-kullanim-sekilleri.py
@@ -5,23 +5,24 @@ print ("Selam WeWantEd\n")
 print ('%r %r %r %r' % (1, 2 , 3 , 4))
 print ('%r %r %r %r' % ("one", "two", "three", "four"))
 
+
 print ("\n\nWeWanted \n"
     "Eğitimlerine \n"
     "Hoşgeldiniz\n")
 
 
 print ("Karabük" + " Üniversitesi" + " Mühendislik" + " Fakültesi\n")
-
 print ("Karabük\t" + "Üniversitesi\t\t" + " Mühendislik\t\t\t" + " Fakültesi")
+
 
 print ("""
 WeWantEd
 Python
-Dersi
+Dersleri
 """)
 
 print ('''
 WeWantEd
 Python
-Dersi
+Dersleri
 ''')
