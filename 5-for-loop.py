74657fd2e6d0d1a0957687a485e2bf4495102f92
Ömer Faruk Kocadayı <omerkocadayi@gmail.com>
2017-03-26 18:35:27 +0300

    Update 5-for-loop.py

--- 5-for-loop.py
+++ 5-for-loop.py
@@ -22,37 +22,3 @@ for x in range(1, 11):
 
     print("\n")
 print("\n---------------------------------------\n")
-
-
-
-# ASAL SAYI BULMA
-
-sayi = int(input('Bir sayi giriniz : '))
-bolundu = 0
-for i in range(2,(sayi+1)//2):
-    if sayi%i == 0:
-        print ("Girdiginiz sayi asal degildir !!")
-        bolundu += 1
-        break
-
-if(bolundu == 0):
-    print ("Bu sayi asaldir !!")
-print("\n---------------------------------------\n")
-
-
-
-
-# ILK N ADET FIBONACCI SAYISINI YAZDIRMA
-
-sayi = int(input('Bir sayi giriniz : '))
-print("Fibonacci sayi dizisinin ilk {} sayisi" .format(sayi))
-
-a, b = 1, 1
-print (a)
-print (b)
-for i in range(2,sayi):
-    a, b = b, a+b
-    print (b)
-    i += 1
-
-print("\n---------------------------------------\n")
