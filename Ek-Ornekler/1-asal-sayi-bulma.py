e0cb129b6b1f83ed7a7f39c7b77aa893166bc32b
Ömer Faruk Kocadayı <omerkocadayi@gmail.com>
2017-03-26 18:51:26 +0300

    Update 1-asal-sayi-bulma.py

--- Ek-Ornekler/1-asal-sayi-bulma.py
+++ Ek-Ornekler/1-asal-sayi-bulma.py
@@ -2,7 +2,9 @@
 
 sayi = int(input('Bir sayi giriniz : '))
 bolundu = 0
-for i in range(2, (sayi+1)//2):
+
+for i in range(2, (sayi//2):
+# Bir sayinin yarısından daha buyuk bir boleni olmayacagi icin, programin daha hizli calismasi acisindan sayi//2'ye kadar dondurduk.
     if sayi%i == 0:
         print ("Girdiginiz sayi asal degildir !!")
         bolundu += 1
